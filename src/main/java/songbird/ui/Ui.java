6c334960409a40a2bd5b1996d049fdc069888981
itsvari <13640661+itsvari@users.noreply.github.com>
2025-01-23 19:54:31 +0800

    Extract UI-handling code to new class

---
+++ src/main/java/songbird/ui/Ui.java
@@ -0,0 +1,52 @@
+package songbird.ui;
+
+import java.util.Scanner;
+
+/**
+ * Handles the user interface of the Songbird chatbot, including input and output.
+ */
+public class Ui {
+    private static final String ASCII_ART_LOGO = """
+               _____                   __    _          __
+              / ___/____  ____  ____ _/ /_  (_)________/ /
+              \\__ \\/ __ \\/ __ \\/ __ `/ __ \\/ / ___/ __  /
+             ___/ / /_/ / / / / /_/ / /_/ / / /  / /_/ /
+            /____/\\____/_/ /_/\\__, /_.___/_/_/   \\__,_/
+                             /____/""";
+    private final Scanner input = new Scanner(System.in);
+
+    /**
+     * Greets the user with the Songbird logo and a welcome message.
+     * This method should be called once at the start of
+     * the program.
+     */
+    public void greet() {
+        System.out.println(ASCII_ART_LOGO);
+        System.out.println("Songbird(TM) AI by VariTech Heavy Industries, (C) 3025. All rights reserved.");
+        System.out.println("--------------------");
+        this.respond("How can I help?");
+    }
+
+    /**
+     * Reads the user's command from the console input.
+     *
+     * @return The user's command.
+     */
+    public String readCommand() {
+        return input.nextLine();
+    }
+
+    /**
+     * Responds to the user with the given messages.
+     * <p>
+     * Each message is printed on a new line and prefixed with "S> ".
+     * Takes in a variable number of messages to respond with, which are printed in the order they are provided.
+     *
+     * @param messages The messages to respond with.
+     */
+    public void respond(String... messages) {
+        for (String message : messages) {
+            System.out.println("S> " + message);
+        }
+    }
+}
