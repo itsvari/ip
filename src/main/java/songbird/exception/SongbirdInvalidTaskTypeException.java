b10393759fe6d42bcef4db3a4659747bf2c1ef31
itsvari <13640661+itsvari@users.noreply.github.com>
2025-01-24 07:12:35 +0800

    Delete unused exception

--- src/main/java/songbird/exception/SongbirdInvalidTaskTypeException.java
+++
@@ -1,7 +0,0 @@
-package songbird.exception;
-
-public class SongbirdInvalidTaskTypeException extends SongbirdException {
-    public SongbirdInvalidTaskTypeException() {
-        super("That's not a valid task type. Please try again.");
-    }
-}
